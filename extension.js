4d3b2793f53d750384619ab78906dfefdba9e717
Jean-Christophe Baptiste <jc.baptiste@sysdream.com>
2016-05-21 22:32:29 +0200

    Fix #12, #19

--- extension.js
+++ extension.js
@@ -1,4 +1,24 @@
-// -*- mode: js; js-indent-level: 4; indent-tabs-mode: nil -*-
+/*
+Copyright (C) phocean <jc@phocean.net>
+
+Credits to:
+ - "ag" for the original extension,
+ - "Mjnaderi" for the Toptray first fork.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 
 const St = imports.gi.St;
 const Main = imports.ui.main;

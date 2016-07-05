6c9f00da1012efdf6092c73c68c4646c511e0366
Francisco Nunes <maxnevesnunes@gmail.com>
2016-07-05 12:26:02 +0100

    Refactored icon size logic
    The icon's original size isn't needed, since we override it with the
    extension's icon size settings. I've also changed how this was applied,
    since feedback from changes in settings weren't instantly visible.

--- extension.js
+++ extension.js
@@ -50,7 +50,7 @@ function enable() {
     settings.connect('changed::icon-saturation', Lang.bind(this, refreshSaturation));
     settings.connect('changed::icon-brightness', Lang.bind(this, refreshBrightnessContrast));
     settings.connect('changed::icon-contrast', Lang.bind(this, refreshBrightnessContrast));
-    settings.connect('changed::icon-size', Lang.bind(this, refreshSize));
+    settings.connect('changed::icon-size', Lang.bind(this, refreshTray));
     settings.connect('changed::icon-spacing', Lang.bind(this, refreshTray));
     settings.connect('changed::tray-pos', Lang.bind(this, refreshPos));
     settings.connect('changed::tray-order', Lang.bind(this, refreshPos));
@@ -78,15 +78,9 @@ function onTrayIconAdded(o, icon, role, delay) {
     let trayPosition = settings.get_string('tray-pos');
     let trayOrder = settings.get_int('tray-order');
     let scaleFactor = St.ThemeContext.get_for_stage(global.stage).scale_factor;
-    icon.set_size(icon.size.width * scaleFactor, icon.size.height * scaleFactor);
 
     // Container
-    let iconContainer = new St.Button({
-            child: icon,
-            visible: false,
-            width: icon.size.width * scaleFactor,
-            height: icon.size.height * scaleFactor
-    });
+    let iconContainer = new St.Button({child: icon, visible: false});
 
     icon.connect("destroy", function() {
         icon.clear_effects();
@@ -263,6 +257,7 @@ function applyOpacity(icon) {
 function applySize(icon, scaleFactor) {
     let iconSize = settings.get_int('icon-size');
     icon.set_size(iconSize * scaleFactor, iconSize * scaleFactor);
+    icon.get_parent().set_size(iconSize * scaleFactor, iconSize * scaleFactor);
 }
 
 // These functions are called by signals on preference change and loop through icons to apply it
@@ -288,13 +283,6 @@ function refreshBrightnessContrast() {
         applyBrightnessContrast(icon);
     }
     refreshTray();
-}
-
-function refreshSize() {
-    let iconSize = settings.get_int('icon-size');
-    for (let i=0; i<icons.length; i++) {
-        applySize(icons[i]);
-    }
 }
 
 function refreshTray() {
